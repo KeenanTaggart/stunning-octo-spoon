77743c8ec6ffa3fda21828d94620189a449a5d26
Keenan Taggart <keenanbtaggart@gmail.com>
2016-12-12 08:25:55 -0800

    Created first header file

---
+++ euler1.hpp
@@ -0,0 +1,9 @@
+/***********************************************************************
+** Author: Keenan Taggart
+** Description: Header file for first X (10?) functions comprising solutions
+** for Euler Project problems
+***********************************************************************/
+
+#include <iostream>
+
+int eulerFirst();
