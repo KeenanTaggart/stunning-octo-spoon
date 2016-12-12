a87c5e1c872cc9eacdbede8f75ff6e6d3b3dedb7
Keenan Taggart <keenanbtaggart@gmail.com>
2016-12-12 14:00:42 -0800

    Created 'main' file
    Created the file containing the 'main' function which will execute each
    exercise's code/solution

---
+++ eulerMain.cpp
@@ -0,0 +1,10 @@
+// Executes the functions which comprise the solutions to each problem/exercise
+
+#include "euler1.hpp"
+#include <iostream>
+
+int main()
+{
+   std::cout << eulerFirst() << std::endl;
+   return 0;
+}
